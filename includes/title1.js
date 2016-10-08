64c062465c8250beaeb23b063ab679b24c9dc33b
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-10-07 19:07:58 -0600

    abstracts

--- includes/title1.js
+++ includes/title1.js
@@ -7,7 +7,7 @@ document.write("</td>");
 
 document.write("<td align=\"center\">");
 document.write("<h1>CyberSafety 2016</h1>");
-document.write("<h3>The First International Workshop on Computational Methods for CyberSafety </h3>");
+document.write("<h3>The First ACM International Workshop on Computational Methods for CyberSafety </h3>");
 document.write("<h3>Co-located with <a target=\"_tab\" href=\"http://cikm2016.cs.iupui.edu/\" >CIKM 2016 </a> </h3>");
 document.write("<h3>October 28, 2016</h3>");
 document.write("<h3>Indianapolis, IN, USA</h3>");
